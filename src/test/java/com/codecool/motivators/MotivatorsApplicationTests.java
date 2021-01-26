99089feb649fed1e0346979be89244731bf84fc3
coryon <illyes.gergely95@gmail.com>
2021-01-26 09:00:55 +0100

    init

---
+++ src/test/java/com/codecool/motivators/MotivatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.motivators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotivatorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
