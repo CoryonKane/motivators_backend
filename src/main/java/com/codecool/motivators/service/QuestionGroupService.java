55486276e7f7e6fe20b8bc6f37821783d718fcc0
coryon <illyes.gergely95@gmail.com>
2021-01-26 20:48:56 +0100

    add: service classes initialized

--- src/main/java/com/codecool/motivators/service/QuestionGroupService.java
+++ src/main/java/com/codecool/motivators/service/QuestionGroupService.java
@@ -1,7 +1,16 @@
 package com.codecool.motivators.service;
 
+import com.codecool.motivators.repository.QuestionGroupRepository;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuestionGroupService {
+    private final DtoConverterService converter;
+    private final QuestionGroupRepository repository;
+
+    public QuestionGroupService(@Lazy DtoConverterService converter, QuestionGroupRepository repository) {
+        this.converter = converter;
+        this.repository = repository;
+    }
 }
--- src/main/java/com/codecool/motivators/service/UserService.java
+++ src/main/java/com/codecool/motivators/service/UserService.java
@@ -1,7 +1,16 @@
 package com.codecool.motivators.service;
 
+import com.codecool.motivators.repository.UserRepository;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final DtoConverterService converter;
+    private final UserRepository repository;
+
+    public UserService(@Lazy DtoConverterService converter, UserRepository repository) {
+        this.converter = converter;
+        this.repository = repository;
+    }
 }
