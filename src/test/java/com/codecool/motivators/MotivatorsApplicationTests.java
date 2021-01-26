efab8a1f5db7befd5b5dd71bee9569e4e5bb3cc7
coryon <illyes.gergely95@gmail.com>
2021-01-26 09:53:15 +0100

    add: cardType enum created without images

--- src/test/java/com/codecool/motivators/MotivatorsApplicationTests.java
+++ src/test/java/com/codecool/motivators/MotivatorsApplicationTests.java
@@ -2,6 +2,10 @@ package com.codecool.motivators;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.Assert.*;
+//import static org.mockito.Mockito.*;
+//import org.junit.jupiter.api.BeforeEach;
+
 
 @SpringBootTest
 class MotivatorsApplicationTests {
