eb581fd70a82ab4c08a732790944dfd9c7f14c9c
coryon <illyes.gergely95@gmail.com>
2021-01-28 18:09:17 +0100

    disable spring security try 2

--- src/main/java/com/codecool/motivators/security/SecurityConfig.java
+++ src/main/java/com/codecool/motivators/security/SecurityConfig.java
@@ -1,16 +1,14 @@
 package com.codecool.motivators.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.authorizeExchange().anyExchange().permitAll();
-        return http.build();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
     }
 }
