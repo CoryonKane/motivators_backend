b8346077fa18d1820c658f09d12375586c5e80f0
coryon <illyes.gergely95@gmail.com>
2021-02-11 19:30:55 +0100

    fix: search now not case sensitive

--- src/main/java/com/codecool/motivators/repository/UserRepository.java
+++ src/main/java/com/codecool/motivators/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.codecool.motivators.repository;
 
 import com.codecool.motivators.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM account u WHERE LOWER(u.name) = LOWER(?1)")
     List<User> findAllByName (String name);
     Optional<User> findByEmail (String email);
 }
